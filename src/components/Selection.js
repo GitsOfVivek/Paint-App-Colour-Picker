87a89ef3a03e5462ea7bbec597789bde51bd1a97
GitsOfVivek <singhvivek1999@yahoo.com>
2023-02-17 01:04:37 +0530

    first

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
+import React, { useState } from 'react';
 
+const Selection = ({ applyColor }) => {
+	const [background, setBackground] = useState('');
+	return (
+		<div
+			className="fix-box"
+			style={{ background: `${background}` }}
+			onClick={() => {}}>
+			<h2 className="subheading">Selection</h2>
+		</div>
+	);
+};
+
+export default Selection;
